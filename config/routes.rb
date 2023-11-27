55bbdb5c0e49222dc60507c3cc0958f9157760f4
Dylan Timmons <dylanstimmons@icloud.com>
2023-11-27 08:37:50 -0700

    adds endpoints for post patch and get

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      post "/customers/:customer_id/subscriptions/:subscription_id", to: "subscription_customers#create"
+      patch "/customers/:customer_id/subscriptions/:subscription_id", to: "subscription_customers#update"
+      get "/customers/:customer_id/subscriptions", to: "subscription_customers#index"
+    end
+  end
 end

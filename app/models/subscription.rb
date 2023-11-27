51850a82d5da2b0659f90040db57f729e5fe0c35
Dylan Timmons <dylanstimmons@icloud.com>
2023-11-27 08:35:14 -0700

    add subscription model and validation

---
+++ app/models/subscription.rb
@@ -0,0 +1,10 @@
+class Subscription < ApplicationRecord
+    validates :title, presence: true
+    validates :price, presence: true
+    validates :frequency, presence: true
+  
+    has_many :subscription_customers
+    has_many :customers, through: :subscription_customers
+    has_many :tea_subscriptions
+    has_many :teas, through: :tea_subscriptions
+end
